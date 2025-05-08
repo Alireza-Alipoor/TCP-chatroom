7ab1941f186a62416c90be2197017fa777d99137
Alireza-Alipoor <thealipoor@gmail.com>
2025-05-08 23:09:53 +0330

    feat: implement receive function

---
+++ client/client.py
@@ -0,0 +1,19 @@
+import socket
+import threading
+
+IP = 'localhost'
+PORT = 1234
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((IP, PORT))
+
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode()
+            print(message)
+        except:
+            print('disconnected')
+            client.close()
+            break
