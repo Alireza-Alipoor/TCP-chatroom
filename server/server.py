801fe8d17366111d258c27254f1284b9031761f0
Alireza-Alipoor <thealipoor@gmail.com>
2025-05-09 11:31:14 +0330

    refactor: show address instead of whole socket, make it more readable

--- server/server.py
+++ server/server.py
@@ -11,19 +11,20 @@ server.listen()
 clients: set[socket.socket] = set()
 
 
-def broadcast(message: str) -> None:
+def broadcast(message: str, sender: socket.socket | None = None) -> None:
     for client in clients:
-        client.send(message.encode())
+        if client != sender:
+            client.send(message.encode())
 
 
-def handle(client: socket.socket) -> None:
+def handle(client: socket.socket, address) -> None:
     while True:
         try:
             message = client.recv(1024).decode()
-            broadcast(f'{client}:{message}')
+            broadcast(f'{address}:{message}', client)
         except:
-            broadcast(f'{client} left the chat.')
-            print(f'{client} left the chat')
+            broadcast(f'{address} left the chat.')
+            print(f'{address} left the chat')
             clients.remove(client)
             client.close()
             break
@@ -33,10 +34,10 @@ def main():
     print(f'server is running on port={PORT}')
     while True:
         client, address = server.accept()
-        print(f'{client} joined the chat')
-        broadcast(f'{client} joined the chat')
+        print(f'{address} joined the chat')
+        broadcast(f'{address} joined the chat')
         clients.add(client)
-        thread = threading.Thread(target=handle, args=(client,))
+        thread = threading.Thread(target=handle, args=(client, address))
         thread.start()
 
 
