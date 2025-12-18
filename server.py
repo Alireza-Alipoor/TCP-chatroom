868869396c9b66d85b1622465c07ece6ff234a2b
Alireza-Alipoor <thealipoor@gmail.com>
2025-12-18 17:57:17 +0330

    chore: format server code and set listen backlog

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 import socket
 import threading
 
-IP = 'localhost'
+IP = "localhost"
 PORT = 1234
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((IP, PORT))
-server.listen()
+server.listen(4)
 
 clients: set[socket.socket] = set()
 
@@ -21,25 +21,26 @@ def handle(client: socket.socket, address) -> None:
     while True:
         try:
             message = client.recv(1024).decode()
-            broadcast(f'{address}:{message}', client)
+            broadcast(f"{address}:{message}", client)
         except:
-            broadcast(f'{address} left the chat.')
-            print(f'{address} left the chat')
+            broadcast(f"{address} left the chat.")
+            print(f"{address} left the chat")
             clients.remove(client)
             client.close()
             break
 
 
 def main():
-    print(f'server is running on port={PORT}')
+    print(f"server is running on port={PORT}")
     while True:
         client, address = server.accept()
-        print(f'{address} joined the chat')
-        broadcast(f'{address} joined the chat')
+        print(f"{address} joined the chat")
+        broadcast(f"{address} joined the chat")
         clients.add(client)
         thread = threading.Thread(target=handle, args=(client, address))
         thread.start()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
